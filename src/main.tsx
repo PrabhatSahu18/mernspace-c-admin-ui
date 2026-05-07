7d82e9644769612fe83b1b481c02f40fd8339f30
Prabhat <c.prabhat18@gmail.com>
2026-05-07 15:44:52 +0530

    api setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,28 @@ import { createRoot } from 'react-dom/client'
 import { ConfigProvider } from 'antd'
 import 'antd/dist/reset.css'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConfigProvider
-        theme={{
-            token: {
-                colorPrimary: '#F65F42',
-                colorLink: '#F65F42',
-            },
-        }}>
-        <RouterProvider router={router} />
-    </ConfigProvider>
+
+    <QueryClientProvider client={queryClient}>
+
+      <ConfigProvider
+          theme={{
+              token: {
+                  colorPrimary: '#F65F42',
+                  colorLink: '#F65F42',
+              },
+          }}>
+          <RouterProvider router={router} />
+      </ConfigProvider>
+
+    </QueryClientProvider>
   </StrictMode>,
 )
 
